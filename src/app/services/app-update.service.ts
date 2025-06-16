a7ecd8374ff252b0f763aeda8e0d8dd1c397a3ba
Nitesh Kesarkar <nitesh.kesarkar@codeblaze.ae>
2025-06-16 17:23:03 +0530

    add debug statements

--- src/app/services/app-update.service.ts
+++ src/app/services/app-update.service.ts
@@ -10,23 +10,28 @@ import { first } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class AppUpdateService {
   constructor(appRef: ApplicationRef, private updates: SwUpdate) {
+    console.log('AppUpdateService initialized');
     if (!updates.isEnabled) return;
 
     const appIsStable$ = appRef.isStable.pipe(first(stable => stable));
-    const everySixHours$ = interval(6 * 60 * 60 * 1000);
-    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+    const everyTenSeconds$ =  interval(10 * 1000); // check every 10 seconds
+    const everyTenSecondsOnceAppIsStable$ = concat(appIsStable$, everyTenSeconds$);
 
-    everySixHoursOnceAppIsStable$.subscribe(() => {
+    everyTenSecondsOnceAppIsStable$.subscribe(() => {
+      console.log('AppUpdateService Checking for app updates...');
       this.updates.checkForUpdate();
     });
 
     this.updates.versionUpdates.subscribe((event: VersionEvent) => {
+      console.log('AppUpdateService versionUpdates...');
       if (event.type === 'VERSION_READY') {
+          console.log('AppUpdateService ready...');
         const versionReadyEvent = event as VersionReadyEvent;
         const update = confirm(
           `A new version is available.\n\nCurrent: ${versionReadyEvent.currentVersion.hash}\nLatest: ${versionReadyEvent.latestVersion.hash}\n\nReload to update?`
         );
         if (update) {
+          console.log('AppUpdateService updating...');
           this.updates.activateUpdate().then(() => document.location.reload());
         }
       }
